78d19745d2e22b07ad297b0adac3626ef4d4c5d2
sahil sarawagi <sahilsarawagi1@gmail.com>
2022-08-25 19:34:14 +0530

    OOP has been added

---
+++ OOP/src/com/Sahil/intro.java
@@ -0,0 +1,35 @@
+package com.Sahil;
+
+public class intro {
+    public static void main(String[] args) {
+        Student Sahil = new Student();
+        System.out.println(Sahil.rno);
+        System.out.println(Sahil.name);
+        Student sakura =new Student();
+        System.out.println(sakura.name +"  "+ sakura.rno);
+        Student random = new Student(Sahil);
+        System.out.println(random.name);
+    }
+    static class Student {
+        String name;
+        int rno;
+        Student(){
+//            this.name = "Sasuke";
+//            this.rno = 8;
+
+            // internally it is something like Student(int rno , String name);
+            this( 18  , "Naruto");
+        }
+        Student(Student other){
+            this.name = other.name;    // this will act as a random.name = Sahil.name
+
+            // this is how u call a constructor from another constructor
+
+        }
+        Student(int rno, String name){
+            this.rno = rno;
+            this.name = name;
+        }
+    }
+}
+
